6b422b8c63233807cedcd96560cb234c4e73ce25
百无一用是书生 <49895012+intellectual-yao@users.noreply.github.com>
2022-02-15 17:21:17 +0800

    Update deal_pic_by_opencv.py
    相比上一个只有翻转的data augmentation,本次增加了旋转、移动、高斯噪声的data augmentation

--- deal_pic_by_opencv.py
+++ deal_pic_by_opencv.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+from PIL import Image, ImageChops, ImageEnhance
+import skimage
 import numpy as np
 from skimage import transform, data
 import matplotlib.pyplot as plt
@@ -7,9 +9,9 @@ import matplotlib.pyplot as plt
 
 # 依次读取文件夹的所有图片
 def read_directory(dir_path):
-    k = 15
+    k = 30
     # C:\Users\intellectual_yao\Desktop\pic\new_resize
-    new_path = "E:\\new_apps\\ViT\\dianluban\\resize_data\\after_deal"
+    new_path = "E:\\new_apps\\ViT\\dianluban\\else\\received_new"
     for img_name in os.listdir(dir_path):
         print(img_name)
         # 读图
@@ -20,25 +22,34 @@ def read_directory(dir_path):
         # print(resize_img.shape)
 
         v_image = cv2.flip(_img, 0)  # 图像垂直翻转
-        resize_v_img = cv2.resize(v_image, (512, 512))
-        print("resize_v_img.shape is: ", resize_v_img.shape)
+        # resize_v_img = cv2.resize(v_image, (512, 512))
+        # print("resize_v_img.shape is: ", resize_v_img.shape)
         k += 1
         save_path = new_path + "\\" + str(k) + ".jpg"
-        cv2.imwrite(save_path, resize_v_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(save_path, v_image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
 
         h_image = cv2.flip(_img, 1)  # 图像水平翻转
-        resize_h_img = cv2.resize(h_image, (512, 512))
-        print("resize_h_img.shape is: ", resize_h_img.shape)
+        # resize_h_img = cv2.resize(h_image, (512, 512))
+        # print("resize_h_img.shape is: ", resize_h_img.shape)
         k += 1
         save_path = new_path + "\\" + str(k) + ".jpg"
-        cv2.imwrite(save_path, resize_h_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(save_path, h_image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
 
         h_v_image = cv2.flip(_img, -1)  # 图像垂直和水平翻转.
-        resize_h_v_img = cv2.resize(h_v_image, (512, 512))
-        print("resize_h_v_img.shape is: ", resize_h_v_img.shape)
+        # resize_h_v_img = cv2.resize(h_v_image, (512, 512))
+        # print("resize_h_v_img.shape is: ", h_v_image.shape)
         k += 1
         save_path = new_path + "\\" + str(k) + ".jpg"
-        cv2.imwrite(save_path, resize_h_v_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(save_path, h_v_image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+
+        h, w = _img.shape[:2]
+        center = (w // 2, h // 2)
+        # 旋转中心坐标，逆时针旋转：45°，缩放因子：1.1
+        M_1 = cv2.getRotationMatrix2D(center, 45, 1.1)
+        rotated_1 = cv2.warpAffine(_img, M_1, (w, h))
+        k += 1
+        save_path = new_path + "\\" + str(k) + ".jpg"
+        cv2.imwrite(save_path, rotated_1, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
 
 
 # 对处理后的图进行二值化
@@ -51,6 +62,28 @@ def threshold_pic(dir_path):
         cv2.namedWindow("bi_img", 0)
         cv2.imshow("bi_img", bi_img)
         cv2.waitKey(0)
+
+
+# 加高斯噪声，平移图片（循环移位）PIL库，因此部分代码需替换
+def move_img(dir_path):
+    k = 132
+    new_path = "E:\\new_apps\\ViT\\dianluban\\else\\moved_without_gap"
+    for img_name in os.listdir(dir_path):
+        print(img_name)
+        img_path = dir_path + "\\" + img_name
+        _img = Image.open(img_path)
+        moved_img = ImageChops.offset(_img, 100, 150)
+        k += 1
+        save_path = new_path + "\\" + str(k) + ".jpg"
+        moved_img.save(save_path)
+
+        # img = np.array(_img) # 分类任务，本数据集加噪声以后变为全黑，故不能使用加噪DA
+        # img_noise = skimage.util.random_noise(img, mode="gaussian")
+        # k += 1
+        # save_path = new_path + "\\" + str(k) + ".jpg"
+        # cv2.imwrite(save_path, img_noise, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        # plt.imshow(img)
+        # plt.show()
 
 
 def deal_pic(pic):
@@ -71,10 +104,11 @@ def deal_pic(pic):
 
 if __name__ == '__main__':
     # C:\Users\intellectual_yao\Desktop\pic\new_pic\1.jpg
-    dir_path = "E:\\new_apps\\ViT\\dianluban\\resize_data\\resize_data"
+    dir_path = "E:\\new_apps\\ViT\\dianluban\\else\\received_new"
     # one_path = "E:\\xiangmu\\BSrc1Not.jpg"
     # read_one(dir_path)
 
-    read_directory(dir_path)
+    # read_directory(dir_path)
     # threshold_pic(dir_path)
+    move_img(dir_path)
 
